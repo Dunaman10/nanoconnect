d7e8158cc3389f7262563e46c27a095ff7752571
Syarif Hidayatullah <dunaman1004@gmail.com>
2026-02-06 19:04:38 +0700

    fix: remove hardcoded supabase service key to resolve github security alert

--- node-functions/api/order/index.js
+++ node-functions/api/order/index.js
@@ -7,11 +7,11 @@ const headers = {
   'Access-Control-Allow-Headers': 'Content-Type, Authorization'
 }
 
-const supabase = createClient(
-  'https://gpjprlcpdizkqcutfwvi.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwanBybGNwZGl6a3FjdXRmd3ZpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDAyNzgzNCwiZXhwIjoyMDg1NjAzODM0fQ.ZRuuQeCPlrHJi4klulxNy4cuQrfgvw21dlH7UQWAeJ8')  
-
 export async function onRequestPost(context) {
+  const supabaseUrl = 'https://gpjprlcpdizkqcutfwvi.supabase.co'
+  const supabaseKey = context.env.SUPABASE_SERVICE_KEY
+  const supabase = createClient(supabaseUrl, supabaseKey)
+
   const orderData = await context.request.json()
   
   if(!orderData.influencer_id || !orderData.sme_id || !orderData.description) {
